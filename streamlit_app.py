26519ad87d4bf64d3318874dc60a601f179c57b5
amilashanaka <amila.s@orderchimps.com>
2023-08-27 19:41:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 # page1.py
-import foo
-foo.hello = 123
+import streamlit as st
+if "shared" not in st.session_state:
+   st.session_state["shared"] = True
 
 # page2.py
-import foo
-st.write(foo.hello)  # If page1 already executed, this should write 123
+import streamlit as st
+st.write(st.session_state["shared"])
+# If page1 already executed, this should write True
